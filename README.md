fa05c957fb30a06c6175973c8bb3f3c6ffc9d7af
Jose Carlos Donayre <josecarlos_dk@hotmail.com>
2020-10-20 11:17:05 -0500

    Agregando paquetes

--- README.md
+++
@@ -1,2 +0,0 @@
-# ezshipping
-EZShipping spring
---
+++ src/test/java/pe/edu/upc/ezshipping/SpringEzshippingApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.ezshipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEzshippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
