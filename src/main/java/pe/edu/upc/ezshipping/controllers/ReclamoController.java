ea92752539b1f40fed1848d1e1f1eb85a95204b7
Jose Carlos Donayre <josecarlos_dk@hotmail.com>
2020-10-31 00:12:29 -0500

    Corrigiendo reclamos

--- src/main/java/pe/edu/upc/ezshipping/controllers/ReclamoController.java
+++ src/main/java/pe/edu/upc/ezshipping/controllers/ReclamoController.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.ezshipping.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,31 +17,35 @@ import pe.edu.upc.ezshipping.models.entities.Reclamo;
 import pe.edu.upc.ezshipping.services.ReclamoService;
 
 @Controller
-@RequestMapping("/realizarReclamos")
+@RequestMapping("/reclamos")
 @SessionAttributes("reclamo")
 public class ReclamoController {
 
 	@Autowired
 	private ReclamoService reclamoService;
-	
+
 	@GetMapping
 	public String index(Model model) {
+		Reclamo reclamo = new Reclamo();
 		try {
 			List<Reclamo> reclamos = reclamoService.findAll();
 			model.addAttribute("reclamos", reclamos);
-		} catch (Exception e) {	
+			model.addAttribute("reclamo", reclamo);
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
 		return "/reclamos/index";
 	}
-	
+
 	// /reclamos/save
 	@PostMapping("save")
 	public String save(@ModelAttribute("reclamo") Reclamo reclamo, SessionStatus status) {
 		try {
+			reclamo.setFechaReclamo(new Date()); //guarda la fecha de actual al ejecutar save()
 			reclamoService.save(reclamo);
 			status.setComplete();
+			// System.out.println(reclamo.getFechaReclamo());
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
