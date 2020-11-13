f824b51e6a9a73ec22a559f7651fabaac77259d1
Jacobo Osorio Durand <jacobood1508@gmail.com>
2020-11-13 14:16:36 -0500

    Update

--- src/main/java/pe/edu/upc/ezshipping/controllers/Filtrar_fecha.java
+++ src/main/java/pe/edu/upc/ezshipping/controllers/Filtrar_fecha.java
@@ -1,5 +1,16 @@
 package pe.edu.upc.ezshipping.controllers;
 
-public class Filtrar_fecha {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/filtrar_fechas")
+public class Filtrar_fecha {
+	
+	@GetMapping
+	public String index(Model model) {
+		return "/filtrar_fecha/index";
+	}
 }
