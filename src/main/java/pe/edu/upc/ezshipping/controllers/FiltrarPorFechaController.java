fd22c19120d239f4e92c4731442d04764bb59c39
Jacobo Osorio Durand <jacobood1508@gmail.com>
2020-11-14 01:22:15 -0500

    Update Controller

--- src/main/java/pe/edu/upc/ezshipping/controllers/FiltrarPorFechaController.java
+++ src/main/java/pe/edu/upc/ezshipping/controllers/FiltrarPorFechaController.java
@@ -1,16 +1,30 @@
 package pe.edu.upc.ezshipping.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.upc.ezshipping.models.entities.Cliente;
+import pe.edu.upc.ezshipping.services.ClienteService;
+
 @Controller
 @RequestMapping("/filtrarPorFecha")
 public class FiltrarPorFechaController {
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	@GetMapping
 	public String index(Model model) {
+		Cliente cliente = new Cliente();
+		try {
+			model.addAttribute("cliente", cliente);
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
 		return "/filtrarPorFecha/index";
 	}
 }
