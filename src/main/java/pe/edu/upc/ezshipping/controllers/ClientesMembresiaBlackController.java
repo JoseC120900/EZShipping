449db664f532b7d04cd79abec16c69e3326c53cc
Keiji Shinoda <k.shinoda.2000@hotmail.com>
2020-11-26 15:01:27 -0500

    Final US14

---
+++ src/main/java/pe/edu/upc/ezshipping/controllers/ClientesMembresiaBlackController.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.ezshipping.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.ezshipping.models.entities.TipoCliente;
+import pe.edu.upc.ezshipping.services.TipoClienteService;
+
+@Controller
+@RequestMapping("/clientesmembresiablack")
+public class ClientesMembresiaBlackController {
+    @Autowired
+    private TipoClienteService tipoClienteService;
+
+
+
+
+    private Integer id=3;
+    @GetMapping
+	public String index(Model model) {
+    	
+		try {
+			Optional<TipoCliente> optional = tipoClienteService.findById(id);
+			
+			if(optional.isPresent()) {
+				model.addAttribute("tipoClientes", optional.get());
+				
+			}
+			else {
+				return "";
+			}
+		}catch (Exception e) {	
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}		
+			
+			return "/clientesmembresiablack/index";
+		
+    }
+
+}
--- src/main/java/pe/edu/upc/ezshipping/controllers/ClientesMembresiaController.java
+++ src/main/java/pe/edu/upc/ezshipping/controllers/ClientesMembresiaController.java
@@ -1,34 +1,18 @@
 package pe.edu.upc.ezshipping.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-import pe.edu.upc.ezshipping.models.entities.TipoCliente;
-import pe.edu.upc.ezshipping.services.TipoClienteService;
-
 @Controller
 @RequestMapping("/clientesmembresia")
 public class ClientesMembresiaController {
-    @Autowired
-    private TipoClienteService tipoClienteService;
-
-    @GetMapping
+	
+	@GetMapping
 	public String index(Model model) {
-		try {
-			List<TipoCliente> tipoClientes = tipoClienteService.findAll();
-			model.addAttribute("tipoClientes", tipoClientes);
-		} catch (Exception e) {	
-			e.printStackTrace();
-			System.err.println(e.getMessage());
-		}
+		
 		return "/clientesmembresia/index";
 	}
-    
-
 }
