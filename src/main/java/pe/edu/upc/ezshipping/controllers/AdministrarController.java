1f14cde1c9cc1fd4e0b880c09ce19c0c2eceb552
Keiji Shinoda <k.shinoda.2000@hotmail.com>
2020-11-27 14:42:25 -0500

    update

---
+++ src/main/java/pe/edu/upc/ezshipping/controllers/AdministrarController.java
@@ -0,0 +1,18 @@
+package pe.edu.upc.ezshipping.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/administrar")
+public class AdministrarController {
+	
+	@GetMapping
+	public String index(Model model) {
+		
+		return "/administrar/index";
+	}
+}
